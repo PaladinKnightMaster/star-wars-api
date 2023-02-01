2e36a4e192d1ec6c7f5dcf265a9d9087bdd6eca7
PaladinKnightMaster <paladinknight.master@gmail.com>
2023-02-01 14:47:31 -0500

    Fixed CSS backgroundImage height on small screen

--- src/App.jsx
+++ src/App.jsx
@@ -93,7 +93,7 @@ function App() {
         style={{
           backgroundImage: `url(${image})`,
         }}
-        className="h-screen w-full bg-cover bg-center sm:max-h-full"
+        className="h-screen w-full bg-cover bg-center sm:h-full"
       >
         <div className="flex items-center justify-center">
           <div onClick={handleRefresh} style={{ cursor: "pointer" }}>
